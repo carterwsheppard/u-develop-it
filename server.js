a0acc7703d485f543b5e4f9e55844303187ac38a
carterwsheppard <carterwsheppard@gmail.com>
2022-07-05 13:34:17 -0400

    changed password

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '561retac',
+      password: 'bootcamp123',
       database: 'election'
     },
     console.log('Connected to the election database.')
